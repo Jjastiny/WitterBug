197921aa54da0b0f9635ff08f7150edacdce56f7
Justin Yang <justinyang@Justins-MBP.attlocal.net>
2017-11-21 18:44:23 -0500

    Test fixing ssh push errors.

--- app/src/androidTest/java/com/isymobilegames/witterbug/ui/twitterAuth/TwitterAuthActivityTest.java
+++ app/src/androidTest/java/com/isymobilegames/witterbug/ui/twitterAuth/TwitterAuthActivityTest.java
@@ -6,10 +6,16 @@ import com.isymobilegames.witterbug.dagger.ComponentHolder;
 
 import org.junit.Test;
 
+import javax.inject.Inject;
+
+import retrofit2.Retrofit;
+
 import static org.junit.Assert.*;
 
 /** Created by justinyang on 11/17/17. */
 public class TwitterAuthActivityTest extends MockedBaseTest {
+  @Inject Retrofit retrofit;
+
   @Test
   public void name() throws Exception {
     getBaseComponent().inject(this);
